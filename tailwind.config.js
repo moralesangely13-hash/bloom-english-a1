2ffc6ee6e8c84b27de9f9458db5401c044fb909f
moralesangely13-hash <moralesangely13@gmail.com>
2026-05-20 16:37:23 -0600

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,12 @@
 export default {
-  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
-  theme: { extend: {} },
+  content: [
+    "./index.html",
+    "./App.jsx",
+    "./main.jsx",
+    "./src/**/*.{js,jsx,ts,tsx}"
+  ],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-};
+}
